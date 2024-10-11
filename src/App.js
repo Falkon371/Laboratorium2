b3d34226b7803fcc6e0648077f08da25aa46f485
Konrad Falkowski <konradfalkow13@gmail.com>
2024-10-11 19:14:48 +0200

    zadanie 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+const _ = require('lodash');
+
+const numbers = [1,2,3,4,5];
+
+function sredniaArytmetyczna(numb){
+  return _.mean(numb)
+}
+
+console.log( "Srednia to: " + sredniaArytmetyczna(numbers))
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+      
       </header>
     </div>
   );
