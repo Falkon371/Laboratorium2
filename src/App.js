2b74e9fd36b7222170ee49ee95c7434ecf081a0c
Konrad Falkowski <konradfalkow13@gmail.com>
2024-10-11 19:25:36 +0200

    zadanie 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ function sredniaArytmetyczna(numb){
   return _.mean(numb)
 }
 
+const minmax = (numb) =>{
+  const min = _.min(numb);
+  const max = _.max(numb);
+  return min, max;
+}
+
+const {min, max} = minmax(numbers);
+
+
 console.log( "Srednia to: " + sredniaArytmetyczna(numbers))
+console.log("Minimalna wartość to: " + min);
+console.log("Maksymalna wartość to: " + max);
+
+
 
 function App() {
   return (
